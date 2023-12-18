7c21862dff16bab8e3a1710ce7f21a87f87e7eaa
수지니 <sssssjin99@naver.com>
2023-12-18 20:18:55 +0900

    [chore]: 프로젝트 세팅

---
+++ src/main/java/dev/hooon/interpark/InterparkApplication.java
@@ -0,0 +1,13 @@
+package dev.hooon.interpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hooon/interpark/InterparkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hooon.interpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
