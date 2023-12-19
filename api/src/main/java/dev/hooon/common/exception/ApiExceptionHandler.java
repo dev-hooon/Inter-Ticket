e0830991f159e245d4ce2038606e1ca6475c6ba7
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-12-19 11:37:20 +0900

    [Feat] : 예외처리 템플릿 구현 (#12)
    * feat: ErrorResponse 구현
    
    * feat: ErrorCode 인터페이스 구현
    
    * chore: core 모듈에 spring-boot-starter 의존성 추가
    
    * feat: ErrorResponseTemplate 구현
    
    * feat: ApiExceptionHandler 구현 및 MethodArgumentNotValidException 핸들러 등록

---
+++ api/src/main/java/dev/hooon/common/exception/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package dev.hooon.common.exception;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+	private static final String ARGUMENT_NOT_VALID_MESSAGE = "잘못된 입력 값 입니다";
+	private static final String ARGUMENT_NOT_VALID_ERROR_CODE = "G_001";
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ErrorResponseTemplate handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e
+	) {
+		log.error("MethodArgumentNotValidException : ", e);
+
+		String message = ARGUMENT_NOT_VALID_MESSAGE;
+		List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+		if (!allErrors.isEmpty()) {
+			message = allErrors.get(0).getDefaultMessage();
+		}
+
+		return new ErrorResponseTemplate(message, ARGUMENT_NOT_VALID_ERROR_CODE);
+	}
+}
