2a248b6561e8fb9ef96a621739d491e52fb2bfd6
수지니 <sssssjin99@naver.com>
2023-12-19 22:17:55 +0900

    [hotfix]: cicd test

---
+++ api/src/test/java/ApiTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApiTest {
+
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+        throw new RuntimeException();
+    }
+}
