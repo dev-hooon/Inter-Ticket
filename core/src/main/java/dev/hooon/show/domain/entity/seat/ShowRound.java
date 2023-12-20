6d5b1c63b0a1fd791720ed534ac3753440a81c57
EunChanNam <namhm23@gmail.com>
2023-12-21 00:49:42 +0900

    fix: ShowRound - startTime 컬럼에 LocalTime 컨버터 추가

--- core/src/main/java/dev/hooon/show/domain/entity/seat/ShowRound.java
+++ core/src/main/java/dev/hooon/show/domain/entity/seat/ShowRound.java
@@ -4,7 +4,10 @@ import static lombok.AccessLevel.*;
 
 import java.time.LocalTime;
 
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,5 +23,6 @@ public class ShowRound {
     private int round;
 
     @Column(name = "seat_start_time")
+    @Convert(converter = Jsr310JpaConverters.LocalTimeConverter.class)
     private LocalTime startTime;
 }
