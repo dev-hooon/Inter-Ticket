13da658ee2834fa47ca8052820d338d845d84900
EunChanNam <namhm23@gmail.com>
2023-12-21 00:51:30 +0900

    feat: Seat 엔티티 더미데이터 sql 구현

---
+++ core/src/main/resources/sql/seat_dummy.sql
@@ -0,0 +1,11 @@
+insert into seat_table(seat_id, seat_show_id, seat_grade, seat_is_seat, seat_row, seat_col, seat_price,
+                       seat_show_date, seat_status, seat_show_round, seat_start_time)
+values (1, 1, 'VIP', true, 'A', 1, 1000, now(), 'AVAILABLE', 2, now());
+insert into seat_table(seat_id, seat_show_id, seat_grade, seat_is_seat, seat_row, seat_col, seat_price,
+                       seat_show_date, seat_status, seat_show_round, seat_start_time)
+values (2, 1, 'VIP', true, 'A', 1, 1000, now(), 'AVAILABLE', 1, now());
+insert into seat_table(seat_id, seat_show_id, seat_grade, seat_is_seat, seat_row, seat_col, seat_price,
+                       seat_show_date, seat_status, seat_show_round, seat_start_time)
+values (3, 1, 'VIP', true, 'A', 1, 1000, now(), 'AVAILABLE', 1, now());
+select *
+from seat_table;
