97909bb7ff2535cfda421911d0bf1dcf92784fe5
EunChanNam <namhm23@gmail.com>
2023-12-21 20:05:56 +0900

    feat: ApiTestSupport 템플릿 구현

---
+++ api/src/test/java/dev/hooon/common/support/ApiTestSupport.java
@@ -0,0 +1,12 @@
+package dev.hooon.common.support;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public abstract class ApiTestSupport extends TestContainerSupport {
+
+}
