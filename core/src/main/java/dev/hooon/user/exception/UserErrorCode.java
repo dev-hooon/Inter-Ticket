23120be7589ef98bf684501b2f0810eb1cac45f2
EunChanNam <namhm23@gmail.com>
2023-12-23 19:42:42 +0900

    feat : UserErrorCode 정의

---
+++ core/src/main/java/dev/hooon/user/exception/UserErrorCode.java
@@ -0,0 +1,15 @@
+package dev.hooon.user.exception;
+
+import dev.hooon.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+	NOT_FOUND_BY_ID("ID 에 해당하는 사용자가 존재하지 않습니다", "U_001");
+
+	private final String message;
+	private final String code;
+}
