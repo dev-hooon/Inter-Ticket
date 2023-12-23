41d27b7583e50e726e19a350729dce5a353573ae
ParkJuHan94 <1004sunye@naver.com>
2023-12-24 03:54:34 +0900

    [feat]: 유저 엔티티 패스워드 추가 & 생성자와 팩토리메서드 구현

--- core/src/main/java/dev/hooon/user/domain/entity/User.java
+++ core/src/main/java/dev/hooon/user/domain/entity/User.java
@@ -13,25 +13,50 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "user_table")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends TimeBaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
 
-    @Column(name = "user_email", nullable = false, unique = true)
-    private String email;
+	@Column(name = "user_email", nullable = false, unique = true)
+	private String email;
 
-    @Column(name = "user_name", nullable = false)
-    private String name;
+	@Column(name = "user_name", nullable = false)
+	private String name;
 
-    @Enumerated(STRING)
-    @Column(name = "user_role", nullable = false)
-    private UserRole userRole;
+	@Column(name = "user_password", nullable = false)
+	private String password;
+
+	@Enumerated(STRING)
+	@Column(name = "user_role", nullable = false)
+	private UserRole userRole;
+
+	private User(
+		String email,
+		String name,
+		String password,
+		UserRole userRole
+	) {
+		this.email = email;
+		this.name = name;
+		this.password = password;
+		this.userRole = userRole;
+	}
+
+	public static User of(
+		String email,
+		String name,
+		String password,
+		UserRole userRole
+	) {
+		return new User(email, name, password, userRole);
+	}
 }
