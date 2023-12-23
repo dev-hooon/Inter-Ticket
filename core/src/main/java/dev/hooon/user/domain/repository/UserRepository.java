aa8dd7f93c0932af46e1402d97aea320a8db756e
EunChanNam <namhm23@gmail.com>
2023-12-23 19:43:01 +0900

    feat : UserRepository 구현 및 findById 메소드 구현

---
+++ core/src/main/java/dev/hooon/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.hooon.user.domain.repository;
+
+import java.util.Optional;
+
+import dev.hooon.user.domain.entity.User;
+
+public interface UserRepository {
+
+	Optional<User> findById(Long id);
+}
