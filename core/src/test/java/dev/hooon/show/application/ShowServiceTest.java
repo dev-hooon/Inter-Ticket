f6f57c959df9e610e890cfc757c2f6db5b66a5ff
수지니 <sssssjin99@naver.com>
2023-12-24 17:43:01 +0900

    [test]: 공연 정보 조회 Application test 추가

--- core/src/test/java/dev/hooon/show/application/ShowServiceTest.java
+++ core/src/test/java/dev/hooon/show/application/ShowServiceTest.java
@@ -1,5 +1,6 @@
 package dev.hooon.show.application;
 
+import static dev.hooon.show.domain.entity.ShowCategory.*;
 import static dev.hooon.show.dto.response.AbleBookingDateRoundResponse.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,7 @@ import static org.mockito.BDDMockito.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +18,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.hooon.common.exception.NotFoundException;
+import dev.hooon.show.domain.entity.Show;
+import dev.hooon.show.domain.entity.ShowPeriod;
+import dev.hooon.show.domain.entity.ShowTime;
+import dev.hooon.show.domain.entity.place.Place;
 import dev.hooon.show.domain.repository.SeatRepository;
+import dev.hooon.show.domain.repository.ShowRepository;
+import dev.hooon.show.dto.ShowMapper;
 import dev.hooon.show.dto.query.SeatDateRoundDto;
 import dev.hooon.show.dto.response.AbleBookingDateRoundResponse;
+import dev.hooon.show.dto.response.ShowDetailsInfoResponse;
 
 @DisplayName("[ShowService 테스트]")
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +38,9 @@ class ShowServiceTest {
 	private ShowService showService;
 	@Mock
 	private SeatRepository seatRepository;
+
+	@Mock
+	private ShowRepository showRepository;
 
 	@Test
 	@DisplayName("[show_id 를 통해 예매 가능한 날짜와 회차 정보를 조회한다]")
@@ -58,4 +71,59 @@ class ShowServiceTest {
 			);
 		}
 	}
+
+	@Test
+	@DisplayName("[show_id 를 통해 공연의 세부 정보를 조회할 수 있다]")
+	void getShowDetailInfoTest() {
+		// given
+		Place place = new Place(
+			1L,
+			"블루스퀘어 신한카드홀",
+			"1544-1591",
+			"서울특별시 용산구 이태원로 294 블루스퀘어(한남동)",
+			"http://www.bluesquare.kr/index.asp"
+		);
+		ShowPeriod showPeriod = new ShowPeriod(
+			LocalDate.of(2023, 10, 10),
+			LocalDate.of(2023, 10, 12)
+		);
+		ShowTime showTime = new ShowTime(
+			150,
+			15
+		);
+		Show show = new Show(
+			1L,
+			"레미제라블",
+			MUSICAL,
+			showPeriod,
+			showTime,
+			"만 8세 이상",
+			300,
+			place
+		);
+
+		given(showRepository.findById(1L))
+			.willReturn(Optional.of(show));
+
+		// when
+		ShowDetailsInfoResponse showDetailsInfoResponse = showService.getShowDetailInfo(1L);
+
+		// then
+		assertEquals(ShowMapper.toShowDetailInfoResponse(show), showDetailsInfoResponse);
+	}
+
+	@Test
+	@DisplayName("[존재하지 않는 show_id 로 공연 조회 시 NotFoundException을 반환한다]")
+	void getShowDetailInfo_WithNoShowId_Test() {
+		// given
+
+		// when
+
+		// then
+		assertThrows(
+			NotFoundException.class,
+			() -> showService.getShowDetailInfo(1L)
+		);
+	}
+
 }
