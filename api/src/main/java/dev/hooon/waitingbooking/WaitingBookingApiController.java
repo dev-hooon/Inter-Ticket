5e9059a558b600b975880ee90a7b92f956bd6ef0
EunChanNam <namhm23@gmail.com>
2023-12-24 16:41:39 +0900

    feat: 예매대기 등록 API 구현

---
+++ api/src/main/java/dev/hooon/waitingbooking/WaitingBookingApiController.java
@@ -0,0 +1,29 @@
+package dev.hooon.waitingbooking;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.hooon.waitingbooking.application.facade.WaitingBookingFacade;
+import dev.hooon.waitingbooking.dto.request.WaitingRegisterRequest;
+import dev.hooon.waitingbooking.dto.response.WaitingRegisterResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class WaitingBookingApiController {
+
+	private final WaitingBookingFacade waitingBookingFacade;
+
+	@PostMapping("/api/waiting_bookings")
+	public ResponseEntity<WaitingRegisterResponse> registerWaitingBooking(
+		@Valid @RequestBody WaitingRegisterRequest request,
+		@RequestParam Long userId // TODO 추후에 인증정보 ArgumentResolver 가 구현되면 수정 예정
+	) {
+		WaitingRegisterResponse waitingRegisterResponse = waitingBookingFacade.registerWaitingBooking(userId, request);
+		return ResponseEntity.ok(waitingRegisterResponse);
+	}
+}
