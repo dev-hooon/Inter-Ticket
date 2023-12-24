babb762b0e5459d00e2d7f5453f66a65ff5dd851
수지니 <sssssjin99@naver.com>
2023-12-24 18:35:09 +0900

    [fix]: conflict 해결

--- core/src/main/java/dev/hooon/show/infrastructure/repository/SeatJpaRepository.java
+++ core/src/main/java/dev/hooon/show/infrastructure/repository/SeatJpaRepository.java
@@ -1,13 +1,21 @@
-package dev.hooon.show.domain.repository;
+package dev.hooon.show.infrastructure.repository;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.hooon.show.domain.entity.seat.Seat;
 import dev.hooon.show.dto.query.SeatDateRoundDto;
 
-public interface SeatRepository {
+public interface SeatJpaRepository extends JpaRepository<Seat, Long> {
 
-	void saveAll(Iterable<Seat> seats);
-
+	@Query("""
+		select distinct
+		new dev.hooon.show.dto.query.SeatDateRoundDto(s.showDate, s.showRound.round, s.showRound.startTime)
+		from Seat s
+		where s.show.id = :showId
+		order by s.showDate, s.showRound.round
+		""")
 	List<SeatDateRoundDto> findSeatDateRoundInfoByShowId(Long showId);
 }
