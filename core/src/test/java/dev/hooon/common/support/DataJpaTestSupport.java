334fabcaf7aa4f2d50047a64e36dc252b085f367
수지니 <sssssjin99@naver.com>
2023-12-24 17:59:55 +0900

    [refactor]: DataJpaTestSupport 옵션 설정 추가

--- core/src/test/java/dev/hooon/common/support/DataJpaTestSupport.java
+++ core/src/test/java/dev/hooon/common/support/DataJpaTestSupport.java
@@ -4,8 +4,10 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Repository;
 
-@DataJpaTest
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Repository.class)) // Adaptor 를 스캔목록에 추가하기 위한 코드
 @AutoConfigureTestDatabase(replace = NONE)
 public abstract class DataJpaTestSupport extends TestContainerSupport {
 }
