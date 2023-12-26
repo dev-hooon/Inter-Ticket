f2987ebb7d58467d6f58368c9e02b974247932dc
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-12-26 19:49:27 +0900

    [feat] : 예매대기 등록 API Controller 구현 (#40)
    * feat: 예매대기 등록 요청 DTO에 검증 어노테이션 추가
    
    * feat: 테스트를 위한 간단한 findAll 쿼리 구현
    
    * feat: 예매대기 등록 API 구현

--- api/src/test/java/dev/hooon/common/support/ApiTestSupport.java
+++ api/src/test/java/dev/hooon/common/support/ApiTestSupport.java
@@ -4,9 +4,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public abstract class ApiTestSupport extends TestContainerSupport {
 
+	protected ObjectMapper objectMapper = new ObjectMapper();
+
+	protected String toJson(Object object) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(object);
+	}
 }
