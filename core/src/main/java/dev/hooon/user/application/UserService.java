9e392110b042b3a187538ca94acccb82f9472975
ParkJuHan94 <1004sunye@naver.com>
2023-12-27 02:18:27 +0900

    [feat]: user 서비스 validation 수정 및 비밀번호 암호화 추가

--- core/src/main/java/dev/hooon/user/application/UserService.java
+++ core/src/main/java/dev/hooon/user/application/UserService.java
@@ -2,26 +2,30 @@ package dev.hooon.user.application;
 
 import static dev.hooon.user.exception.UserErrorCode.*;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dev.hooon.auth.entity.EncryptHelper;
 import dev.hooon.common.exception.NotFoundException;
 import dev.hooon.user.domain.entity.User;
-import dev.hooon.user.domain.entity.UserMapper;
 import dev.hooon.user.domain.repository.UserRepository;
-import dev.hooon.user.dto.request.UserJoinRequestDto;
-import dev.hooon.user.exception.UserException;
+import dev.hooon.user.dto.request.UserJoinRequest;
+import dev.hooon.common.exception.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final EncryptHelper encryptHelper;
+
+	private void validateDuplicateEmail(String email) {
+		if (userRepository.findByEmail(email).isPresent()) {
+			throw new ValidationException(DUPLICATED_EMAIL);
+		}
+	}
 
 	public User getUserById(Long userId) {
 		return userRepository.findById(userId)
@@ -29,17 +33,14 @@ public class UserService {
 	}
 
 	@Transactional
-	public Long join(UserJoinRequestDto userJoinRequestDto) {
-		validateDuplicateUser(userJoinRequestDto);
-		User user = UserMapper.toBuyerUser(userJoinRequestDto);
+	public Long join(UserJoinRequest userJoinRequest) {
+		validateDuplicateEmail(userJoinRequest.email());
+		String email = userJoinRequest.email();
+		String name = userJoinRequest.name();
+		String password = userJoinRequest.password();
+		String encryptedPassword = encryptHelper.encrypt(password);
+		User user = User.ofBuyer(email, name, encryptedPassword);
 		return userRepository.save(user);
 	}
 
-	private void validateDuplicateUser(UserJoinRequestDto userJoinRequestDto) {
-		List<User> findUsers = userRepository.findByName(userJoinRequestDto.name());
-		if (!findUsers.isEmpty()) {
-			log.info(DUPLICATED_USER.getMessage());
-			throw new UserException(DUPLICATED_USER);
-		}
-	}
 }
