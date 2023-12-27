d04118c94d60359a18786aaf0fb3e193906d1ae9
수지니 <sssssjin99@naver.com>
2023-12-27 16:53:00 +0900

    [feat]: 공연 좌석 조회 - SeatRepository, SeatRepositoryAdaptor 추가

--- core/src/main/java/dev/hooon/show/domain/repository/SeatRepository.java
+++ core/src/main/java/dev/hooon/show/domain/repository/SeatRepository.java
@@ -1,13 +1,32 @@
 package dev.hooon.show.domain.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+
 import dev.hooon.show.domain.entity.seat.Seat;
+import dev.hooon.show.domain.entity.seat.SeatGrade;
 import dev.hooon.show.dto.query.SeatDateRoundDto;
+import dev.hooon.show.dto.response.seats.SeatsDetailDto;
+import dev.hooon.show.dto.response.seats.SeatsInfoDto;
 
 public interface SeatRepository {
 
 	void saveAll(Iterable<Seat> seats);
 
 	List<SeatDateRoundDto> findSeatDateRoundInfoByShowId(Long showId);
+
+	List<SeatsInfoDto> findSeatInfoByShowIdAndDateAndRound(
+		@Param("showId") Long showId,
+		@Param("date") LocalDate date,
+		@Param("round") int round
+	);
+
+	List<SeatsDetailDto> findSeatsByShowIdAndDateAndRoundAndGrade(
+		@Param("showId") Long showId,
+		@Param("date") LocalDate date,
+		@Param("round") int round,
+		@Param("grade") SeatGrade grade
+	);
 }
--- core/src/main/java/dev/hooon/show/infrastructure/adaptor/SeatRepositoryAdaptor.java
+++ core/src/main/java/dev/hooon/show/infrastructure/adaptor/SeatRepositoryAdaptor.java
@@ -1,12 +1,16 @@
 package dev.hooon.show.infrastructure.adaptor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import dev.hooon.show.domain.entity.seat.Seat;
+import dev.hooon.show.domain.entity.seat.SeatGrade;
 import dev.hooon.show.domain.repository.SeatRepository;
 import dev.hooon.show.dto.query.SeatDateRoundDto;
+import dev.hooon.show.dto.response.seats.SeatsDetailDto;
+import dev.hooon.show.dto.response.seats.SeatsInfoDto;
 import dev.hooon.show.infrastructure.repository.SeatJpaRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -24,5 +28,19 @@ public class SeatRepositoryAdaptor implements SeatRepository {
 	@Override
 	public List<SeatDateRoundDto> findSeatDateRoundInfoByShowId(Long showId) {
 		return seatJpaRepository.findSeatDateRoundInfoByShowId(showId);
+	}
+
+	@Override
+	public List<SeatsInfoDto> findSeatInfoByShowIdAndDateAndRound(
+		Long showId, LocalDate date, int round
+	) {
+		return seatJpaRepository.findSeatInfoByShowIdAndDateAndRound(showId, date, round);
+	}
+
+	@Override
+	public List<SeatsDetailDto> findSeatsByShowIdAndDateAndRoundAndGrade(
+		Long showId, LocalDate date, int round, SeatGrade grade
+	) {
+		return seatJpaRepository.findSeatsByShowIdAndDateAndRoundAndGrade(showId, date, round, grade);
 	}
 }
