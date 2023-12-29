91c846dd3402aedd8bfe18d2c8a1a9d5c7215175
EunChanNam <namhm23@gmail.com>
2023-12-29 21:10:33 +0900

    feat : activateWaitingBooking 메소드 변경감지를 통한 업데이트로 변경 & getWaitingBookingsByStatusIsActivation, expireActiveWaitingBooking 메소드 구현

--- core/src/main/java/dev/hooon/waitingbooking/application/WaitingBookingService.java
+++ core/src/main/java/dev/hooon/waitingbooking/application/WaitingBookingService.java
@@ -1,5 +1,6 @@
 package dev.hooon.waitingbooking.application;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import dev.hooon.user.domain.entity.User;
 import dev.hooon.waitingbooking.domain.entity.WaitingBooking;
+import dev.hooon.waitingbooking.domain.entity.WaitingStatus;
 import dev.hooon.waitingbooking.domain.repository.WaitingBookingRepository;
 import dev.hooon.waitingbooking.dto.request.WaitingRegisterRequest;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +26,27 @@ public class WaitingBookingService {
 		return waitingBooking;
 	}
 
-	// 대기 상태인 WaitingBooking 조회
+	// 대기 상태인 WaitingBooking 조회(fetch join selectedSeat + user)
 	public List<WaitingBooking> getWaitingBookingsByStatusIsWaiting() {
-		return waitingBookingRepository.findByStatusIsWaiting();
+		return waitingBookingRepository.findWithSelectedSeatsByStatus(WaitingStatus.WAITING);
 	}
 
-	// ID 에 해당하는 WaitingBooking ACTIVATION 상태로 변경하고 expireAt 6시간뒤로 설정
+	// WaitingBooking 상태를 ACTIVATION 상태로 변경하고 expiredAt 6시간뒤로 설정한 후 확정 좌석정보를 저장
 	@Transactional
-	public void activateWaitingBooking(Long waitingBookingId) {
-		waitingBookingRepository.updateToActiveById(waitingBookingId);
+	public void activateWaitingBooking(Long waitingBookingId, List<Long> confirmedSeatIds) {
+		waitingBookingRepository.findById(waitingBookingId)
+			.ifPresent(waitingBooking -> waitingBooking.toActive(confirmedSeatIds));
+	}
+
+	// 활성 상태인 WaitingBooking 조회(fetch join confirmedSeat)
+	@Transactional(readOnly = true)
+	public List<WaitingBooking> getWaitingBookingsByStatusIsActivation() {
+		return waitingBookingRepository.findWithConfirmedSeatsByStatus(WaitingStatus.ACTIVATION);
+	}
+
+	// 활성 상태인 WaitingBooking 을 만료상태로 바꿈
+	@Transactional
+	public void expireActiveWaitingBooking(Collection<Long> targetIds) {
+		waitingBookingRepository.updateStatusByIdIn(WaitingStatus.EXPIRED, targetIds);
 	}
 }
