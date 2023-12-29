fd6cf89060931d494aca1625ca6bdf34321b2a6d
EunChanNam <namhm23@gmail.com>
2023-12-29 20:15:34 +0900

    feat : 예약대기시 확정좌석에 사용되는 ConfirmedSeat 엔티티 추가(기존 WaitingBookingSeat 추상화해서 적용)
    * 기존의 WaitingBookingSeat 은 SelectedSeat 으로 전환

---
+++ core/src/main/java/dev/hooon/waitingbooking/domain/entity/waitingbookingseat/SelectedSeat.java
@@ -0,0 +1,25 @@
+package dev.hooon.waitingbooking.domain.entity.waitingbookingseat;
+
+import dev.hooon.waitingbooking.domain.entity.WaitingBooking;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "selected_seat_table")
+@NoArgsConstructor
+@DiscriminatorValue("selected")
+public class SelectedSeat extends WaitingBookingSeat {
+
+	private SelectedSeat(Long seatId, WaitingBooking waitingBooking) {
+		super(seatId, waitingBooking);
+	}
+
+	// 팩토리 메소드
+	public static SelectedSeat of(Long seatId, WaitingBooking waitingBooking) {
+		return new SelectedSeat(seatId, waitingBooking);
+	}
+}
