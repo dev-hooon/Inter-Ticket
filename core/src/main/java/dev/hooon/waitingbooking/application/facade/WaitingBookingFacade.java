91230aff8a98776f705205ba764196083c623be4
EunChanNam <namhm23@gmail.com>
2024-01-01 16:22:08 +0900

    feat : 예약대기 활성화 알림 메일발송 이벤트 적용

--- core/src/main/java/dev/hooon/waitingbooking/application/facade/WaitingBookingFacade.java
+++ core/src/main/java/dev/hooon/waitingbooking/application/facade/WaitingBookingFacade.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import dev.hooon.show.application.SeatService;
@@ -14,6 +15,7 @@ import dev.hooon.waitingbooking.application.WaitingBookingService;
 import dev.hooon.waitingbooking.domain.entity.WaitingBooking;
 import dev.hooon.waitingbooking.dto.request.WaitingRegisterRequest;
 import dev.hooon.waitingbooking.dto.response.WaitingRegisterResponse;
+import dev.hooon.waitingbooking.event.WaitingBookingActiveEvent;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -23,6 +25,7 @@ public class WaitingBookingFacade {
 	private final WaitingBookingService waitingBookingService;
 	private final UserService userService;
 	private final SeatService seatService;
+	private final ApplicationEventPublisher eventPublisher;
 
 	// 선택한 좌석중에서 취소좌석에 포함되는 좌석 ID 를 LIST 로 응답
 	private List<Long> fetchMatchingSeatIds(
@@ -57,27 +60,26 @@ public class WaitingBookingFacade {
 		// 2. 대기중 상태인 예약대기 목록을 날짜순으로 조회한다
 		List<WaitingBooking> waitingList = waitingBookingService.getWaitingBookingsByStatusIsWaiting();
 		// 3. waitingList 반복하면서 아래 작업을 수행
-		/*
-		 * (1) 대기목록에 포함된 좌석의 PK 가 취소된 좌석 Set 에 존재하는지 확인
-		 * (2) 대기목록의 좌석중에서 취소목록에 포함되는 좌석 아이디 가져옴
-		 * (3) 가져온 좌석 아이디 사이즈가 선택좌석개수와 같으면 취소좌석 SET 에서 해당 PK 지우고 좌석 상태를 취소에서 대기로 바꾸고 예약대기를 활성화
-		 *  + 사용자에게 메일 발송 (구현 예정)
-		 */
 		waitingList.forEach(waitingBooking -> {
-			// (1)
+			// (1) 대기목록에 포함된 좌석의 PK 가 취소된 좌석 Set 에 존재하는지 확인
 			List<Long> selectedSeatIds = waitingBooking.getSelectedSeatIds();
-			// (2)
+			// (2) 대기목록의 좌석중에서 취소목록에 포함되는 좌석 아이디 가져옴
 			List<Long> matchSeatIds = fetchMatchingSeatIds(
 				canceledSeatIds,
 				selectedSeatIds,
 				waitingBooking.getSeatCount()
 			);
-			// (3)
+			// (3) 가져온 좌석 아이디 사이즈가 선택좌석개수와 같으면 취소좌석 SET 에서 해당 PK 지우고 좌석 상태를 취소에서 대기로 바꾸고 예약대기를 활성화
 			if (matchSeatIds.size() == waitingBooking.getSeatCount()) {
 				matchSeatIds.forEach(canceledSeatIds::remove);
 				seatService.updateSeatToWaiting(matchSeatIds);
 				waitingBookingService.activateWaitingBooking(waitingBooking.getId(), matchSeatIds);
-				// 메일 알림 이벤트 발행
+				// (4) 사용자에게 메일 발송
+				eventPublisher.publishEvent(new WaitingBookingActiveEvent(
+					waitingBooking.getUser().getName(),
+					waitingBooking.getUser().getEmail(),
+					matchSeatIds.get(0))
+				);
 			}
 		});
 		// 4. 반복이 끝났는데 남아있는 취소 좌석들은 예약가능 상태로 변경
--- scheduler/src/test/java/dev/hooon/waitingbooking/scheduler/WaitingBookingSchedulerTest.java
+++ scheduler/src/test/java/dev/hooon/waitingbooking/scheduler/WaitingBookingSchedulerTest.java
@@ -1,7 +1,9 @@
 package dev.hooon.waitingbooking.scheduler;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,11 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.event.RecordApplicationEvents;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.hooon.common.fixture.SeatFixture;
 import dev.hooon.common.fixture.WaitingBookingFixture;
 import dev.hooon.common.support.TestContainerSupport;
+import dev.hooon.mail.event.WaitingBookingMailEventListener;
 import dev.hooon.show.domain.entity.seat.Seat;
 import dev.hooon.show.domain.entity.seat.SeatStatus;
 import dev.hooon.show.domain.repository.SeatRepository;
@@ -25,11 +30,14 @@ import dev.hooon.user.domain.repository.UserRepository;
 import dev.hooon.waitingbooking.domain.entity.WaitingBooking;
 import dev.hooon.waitingbooking.domain.entity.WaitingStatus;
 import dev.hooon.waitingbooking.domain.repository.WaitingBookingRepository;
+import dev.hooon.waitingbooking.event.WaitingBookingActiveEvent;
+import jakarta.mail.MessagingException;
 
 @DisplayName("[WaitingBookingScheduler 테스트]")
 @SpringBootTest
 @AutoConfigureTestEntityManager
 @Transactional
+@RecordApplicationEvents
 class WaitingBookingSchedulerTest extends TestContainerSupport {
 
 	@Autowired
@@ -42,6 +50,8 @@ class WaitingBookingSchedulerTest extends TestContainerSupport {
 	private SeatRepository seatRepository;
 	@Autowired
 	private TestEntityManager entityManager;
+	@MockBean
+	private WaitingBookingMailEventListener eventListener;
 
 	private void assertSeatStatus(Long id, SeatStatus expected) {
 		assertThat(seatRepository.findById(id)).isPresent()
@@ -59,7 +69,7 @@ class WaitingBookingSchedulerTest extends TestContainerSupport {
 
 	@Test
 	@DisplayName("[현재 대기중인 예약대기를 처리한다]")
-	void scheduleWaitingBookingProcess() {
+	void scheduleWaitingBookingProcess() throws MessagingException, UnsupportedEncodingException {
 		//given
 		List<Seat> seats = List.of(
 			SeatFixture.getSeat(SeatStatus.CANCELED),
@@ -103,6 +113,8 @@ class WaitingBookingSchedulerTest extends TestContainerSupport {
 		assertWaitingBookingStatus(waitingBookings.get(0).getId(), WaitingStatus.ACTIVATION);
 		assertWaitingBookingStatus(waitingBookings.get(1).getId(), WaitingStatus.ACTIVATION);
 		assertWaitingBookingStatus(waitingBookings.get(2).getId(), WaitingStatus.WAITING);
+
+		verify(eventListener, times(2)).sendWaitingBookingMail(any(WaitingBookingActiveEvent.class));
 	}
 
 	@Test
