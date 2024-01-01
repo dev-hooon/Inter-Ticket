6b7ac8d403d2217d91f0e35a00ffb831eb06ed57
EunChanNam <namhm23@gmail.com>
2024-01-01 16:21:37 +0900

    feat : 예약대기 활성화 이벤트를 수신해서 메일을 발송하는 이벤트 리스너 구현

--- core/src/main/java/dev/hooon/show/exception/ShowErrorCode.java
+++ core/src/main/java/dev/hooon/show/exception/ShowErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ShowErrorCode implements ErrorCode {
 
-	SHOW_NOT_FOUND("공연을 찾을 수 없습니다.", "S_001");
+	SHOW_NOT_FOUND("공연을 찾을 수 없습니다.", "S_001"),
+	SHOW_NAME_NOT_FOUND("좌석에 해당하는 공연의 이름을 찾을 수 없습니다", "S_002");
 
 	private final String message;
 	private final String code;
