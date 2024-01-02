59c89c30f3353bc1fb14783b064cc2eee3eb7984
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2024-01-02 15:03:02 +0900

    [chore] : 스케줄러에 shedlock 설정 추가 및 적용 (#52)
    * chore : 모듈별 프로퍼티파일 설정 추가
    
    * chore : shedlock, redis 의존성 추가
    
    * chore : shedlock 설정 추가 및 적용

--- scheduler/src/main/java/dev/hooon/SchedulerApplication.java
+++ scheduler/src/main/java/dev/hooon/SchedulerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-core,application,application-scheduler");
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
 }
