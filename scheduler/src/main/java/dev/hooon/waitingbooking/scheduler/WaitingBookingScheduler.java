1845a79d27775842b42a834c3863402b9a8ea8ef
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2024-01-02 15:03:02 +0900

    [chore] : 스케줄러에 shedlock 설정 추가 및 적용 (#52)
    * chore : 모듈별 프로퍼티파일 설정 추가
    
    * chore : shedlock, redis 의존성 추가
    
    * chore : shedlock 설정 추가 및 적용

--- scheduler/src/main/java/dev/hooon/waitingbooking/scheduler/WaitingBookingScheduler.java
+++ scheduler/src/main/java/dev/hooon/waitingbooking/scheduler/WaitingBookingScheduler.java
@@ -3,9 +3,13 @@ package dev.hooon.waitingbooking.scheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+
 import dev.hooon.waitingbooking.application.facade.WaitingBookingFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WaitingBookingScheduler {
@@ -13,12 +17,16 @@ public class WaitingBookingScheduler {
 	private final WaitingBookingFacade waitingBookingFacade;
 
 	@Scheduled(cron = "0 0/10 * * * *")
+	@SchedulerLock(name = "wb_1", lockAtLeastFor = "9m", lockAtMostFor = "15m")
 	public void scheduleWaitingBookingProcess() {
+		log.info("wb_1 : run");
 		waitingBookingFacade.processWaitingBooking();
 	}
 
 	@Scheduled(cron = "0/5 * * * * *")
+	@SchedulerLock(name = "wb_2", lockAtLeastFor = "4s", lockAtMostFor = "8s")
 	public void scheduleExpiredWaitingBookingProcess() {
+		log.info("wb_2 : run : run");
 		waitingBookingFacade.processExpiredWaitingBooking();
 	}
 }
