77affe885e4afd19b32857c23d6eb82dc6a8a8a7
ParkJuHan <1004sunye@naver.com>
2024-01-02 11:34:34 +0900

    Revert "[feat] : 회원가입 api 구현 & use 엔티티 추가 구현 (#41)"
    This reverts commit d438902e4388846a0d5df7e07466fa3fb941033a.

--- core/src/main/java/dev/hooon/user/domain/repository/UserRepository.java
+++ core/src/main/java/dev/hooon/user/domain/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package dev.hooon.user.domain.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import dev.hooon.user.domain.entity.User;
@@ -10,9 +9,4 @@ public interface UserRepository {
 	User save(User user);
 
 	Optional<User> findById(Long id);
-
-	Optional<User> findByName(String name);
-	Long save(User user);
-
-	Optional<User> findByEmail(String email);
 }
--- core/src/main/java/dev/hooon/user/exception/UserErrorCode.java
+++ core/src/main/java/dev/hooon/user/exception/UserErrorCode.java
@@ -8,8 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-	NOT_FOUND_BY_ID("ID 에 해당하는 사용자가 존재하지 않습니다", "U_001"),
-	DUPLICATED_EMAIL("이미 존재하는 이메일입니다. 회원가입을 다시 진행해주세요.", "U_002");
+	NOT_FOUND_BY_ID("ID 에 해당하는 사용자가 존재하지 않습니다", "U_001");
 
 	private final String message;
 	private final String code;
