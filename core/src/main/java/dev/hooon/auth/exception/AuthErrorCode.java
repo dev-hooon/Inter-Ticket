684657c47102763bed58259695d2967fdd97fb62
ParkJuHan94 <1004sunye@naver.com>
2024-01-03 15:07:08 +0900

    [feat]: Auth errorcode, Auth exception 구현

---
+++ core/src/main/java/dev/hooon/auth/exception/AuthErrorCode.java
@@ -0,0 +1,19 @@
+package dev.hooon.auth.exception;
+
+import dev.hooon.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+
+	FAILED_LOGIN_BY_ANYTHING("등록되지 않은 아이디이거나 아이디 또는 비밀번호를 잘못 입력했습니다.", "A_001"),
+	NOT_PERMITTED_USER("해당 요청에 대한 권한이 없습니다.", "A_002"),
+	INVALID_ACCESS_TOKEN("유효하지 않은 액세스 토큰입니다.", "A_003"),
+	INVALID_REFRESH_TOKEN("유효하지 않은 리프레쉬 토큰입니다.", "A_004"),
+	NOT_FOUND_REFRESH_TOKEN("해당 리프레쉬 토큰이 존재하지 않습니다.", "A_005");
+
+	private final String message;
+	private final String code;
+}
