eed29d254c797cba46bd87a224100226ab2a1528
ParkJuHan94 <1004sunye@naver.com>
2024-01-03 15:05:15 +0900

    [feat]: Auth 엔티티 구현

---
+++ core/src/main/java/dev/hooon/auth/domain/entity/Auth.java
@@ -0,0 +1,38 @@
+package dev.hooon.auth.domain.entity;
+
+import static jakarta.persistence.GenerationType.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "auth_table")
+public class Auth {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "auth_id")
+	private Long id;
+
+	@Column(name = "auth_user_id", nullable = false, unique = true)
+	private Long userId;
+
+	@Column(name = "auth_refresh_token", nullable = false, unique = true)
+	private String refreshToken;
+
+	private Auth(Long userId, String refreshToken) {
+		this.userId = userId;
+		this.refreshToken = refreshToken;
+	}
+
+	public static Auth of(Long userId, String refreshToken) {
+		return new Auth(userId, refreshToken);
+	}
+}
