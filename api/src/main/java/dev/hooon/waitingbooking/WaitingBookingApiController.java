07403af559b1098bfb1204fc8b1ff28e2c878364
ParkJuHan94 <1004sunye@naver.com>
2024-01-06 12:31:27 +0900

    [refactor]: WaitingBookingApiController 에 argument resolver 리팩토링 & WaitingBookingApiControllerTest 트러블 슈팅해서 리팩토링

--- api/src/main/java/dev/hooon/waitingbooking/WaitingBookingApiController.java
+++ api/src/main/java/dev/hooon/waitingbooking/WaitingBookingApiController.java
@@ -3,9 +3,10 @@ package dev.hooon.waitingbooking;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.hooon.auth.JwtAuthorization;
+import dev.hooon.auth.NoAuth;
 import dev.hooon.waitingbooking.application.facade.WaitingBookingFacade;
 import dev.hooon.waitingbooking.dto.request.WaitingRegisterRequest;
 import dev.hooon.waitingbooking.dto.response.WaitingRegisterResponse;
@@ -18,10 +19,11 @@ public class WaitingBookingApiController {
 
 	private final WaitingBookingFacade waitingBookingFacade;
 
+	@NoAuth
 	@PostMapping("/api/waiting_bookings")
 	public ResponseEntity<WaitingRegisterResponse> registerWaitingBooking(
 		@Valid @RequestBody WaitingRegisterRequest request,
-		@RequestParam Long userId // TODO 추후에 인증정보 ArgumentResolver 가 구현되면 수정 예정
+		@JwtAuthorization Long userId
 	) {
 		WaitingRegisterResponse waitingRegisterResponse = waitingBookingFacade.registerWaitingBooking(userId, request);
 		return ResponseEntity.ok(waitingRegisterResponse);
