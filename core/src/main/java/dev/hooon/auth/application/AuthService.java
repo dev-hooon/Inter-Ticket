831ab312c2c04763b1884f8185d9cfec65f58022
ParkJuHan94 <1004sunye@naver.com>
2024-01-06 12:25:16 +0900

    [feat]: AuthService 로직 변경 & AuthServiceTest 트러블슈팅

--- core/src/main/java/dev/hooon/auth/application/AuthService.java
+++ core/src/main/java/dev/hooon/auth/application/AuthService.java
@@ -1,25 +1,18 @@
 
 package dev.hooon.auth.application;
 
-import static dev.hooon.auth.domain.entity.TokenType.*;
 import static dev.hooon.auth.exception.AuthErrorCode.*;
-
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.hooon.auth.domain.entity.Auth;
-import dev.hooon.auth.domain.entity.TokenType;
 import dev.hooon.auth.domain.repository.AuthRepository;
 import dev.hooon.auth.dto.request.AuthRequest;
 import dev.hooon.auth.dto.response.AuthResponse;
 import dev.hooon.auth.entity.EncryptHelper;
 import dev.hooon.common.exception.NotFoundException;
 import dev.hooon.user.application.UserService;
-import dev.hooon.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -31,55 +24,31 @@ public class AuthService {
 	private final AuthRepository authRepository;
 	private final EncryptHelper encryptHelper;
 
-	private Auth getAuthByRefreshToken(String refreshToken) {
+	public Auth getAuthByRefreshToken(String refreshToken) {
 		return authRepository.findByRefreshToken(refreshToken)
 			.orElseThrow(() -> new NotFoundException(NOT_FOUND_REFRESH_TOKEN));
 	}
 
-	private Map<TokenType, String> createTokensWhenLogin(User user) {
-		Map<TokenType, String> tokens = new EnumMap<>(TokenType.class);
-		String accessToken = jwtProvider.createToken(user, ACCESS);
-		String refreshToken = jwtProvider.createToken(user, REFRESH);
-		tokens.put(ACCESS, accessToken);
-		tokens.put(REFRESH, refreshToken);
-
-		Long userId = user.getId();
-		Optional<Auth> auth = authRepository.findByUserId(userId);
-
-		auth.ifPresentOrElse(
-			(none) -> authRepository.updateRefreshToken(auth.get().getId(), refreshToken),
-			() -> {
-				Auth newAuth = Auth.of(userId, refreshToken);
-				authRepository.save(newAuth);
-			}
-		);
-
-		return tokens;
-	}
-
 	@Transactional
 	public AuthResponse login(AuthRequest authRequest) {
-		User user = userService.getUserByEmail(authRequest.email());
-		Map<TokenType, String> tokens = createTokensWhenLogin(user);
-
+		Long userId = userService.getUserByEmail(authRequest.email()).getId();
+		String[] tokensWhenLogin = jwtProvider.createTokensWhenLogin(userId);
 		String plainPassword = authRequest.password();
-		String hashedPassword = userService.getUserById(user.getId()).getPassword();
+		String hashedPassword = userService.getUserById(userId).getPassword();
 
 		if (encryptHelper.isMatch(plainPassword, hashedPassword)) {
 			return AuthResponse.of(
-				user.getName(),
-				user.getEmail(),
-				tokens.get(ACCESS),
-				tokens.get(REFRESH)
+				tokensWhenLogin[0],
+				tokensWhenLogin[1]
 			);
 		}
 		throw new NotFoundException(FAILED_LOGIN_BY_ANYTHING);
 	}
 
-	public String generateAccessTokenFromRefreshToken(String refreshToken) {
+	public String createAccessTokenByRefreshToken(String refreshToken) {
 		Auth auth = getAuthByRefreshToken(refreshToken);
-		User user = userService.getUserById(auth.getUserId());
-		return jwtProvider.createToken(user, ACCESS);
+		Long userId = userService.getUserById(auth.getUserId()).getId();
+		return jwtProvider.createAccessToken(userId);
 	}
 
 }
--- core/src/test/java/dev/hooon/auth/application/AuthServiceTest.java
+++ core/src/test/java/dev/hooon/auth/application/AuthServiceTest.java
@@ -1,6 +1,5 @@
 package dev.hooon.auth.application;
 
-import static dev.hooon.auth.domain.entity.TokenType.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -11,10 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import dev.hooon.auth.domain.entity.Auth;
-import dev.hooon.auth.domain.repository.AuthRepository;
 import dev.hooon.auth.dto.request.AuthRequest;
 import dev.hooon.auth.dto.response.AuthResponse;
+import dev.hooon.auth.entity.EncryptHelper;
 import dev.hooon.common.exception.NotFoundException;
 import dev.hooon.user.application.UserService;
 import dev.hooon.user.domain.entity.User;
@@ -26,22 +24,23 @@ class AuthServiceTest {
 	@InjectMocks
 	private AuthService authService;
 	@Mock
-	private AuthRepository authRepository;
-	@Mock
 	private UserService userService;
 	@Mock
 	private JwtProvider jwtProvider;
+	@Mock
+	private EncryptHelper encryptHelper;
 
 	@Test
 	@DisplayName("[로그인 성공 시 토큰을 발급한다]")
 	void createTokensWhenLoginSuccessTest() {
 		// given
 		AuthRequest authRequest = new AuthRequest("user@example.com", "password");
-		User user = User.ofBuyer("user@example.com", "name", "password");
+		User user = User.ofBuyer(1L, "user@example.com", "name", "password");
 
 		when(userService.getUserByEmail(authRequest.email())).thenReturn(user);
-		when(jwtProvider.createToken(user, ACCESS)).thenReturn("access-token");
-		when(jwtProvider.createToken(user, REFRESH)).thenReturn("refresh-token");
+		when(userService.getUserById(1L)).thenReturn(user);
+		when(encryptHelper.isMatch(anyString(), anyString())).thenReturn(true);
+		when(jwtProvider.createTokensWhenLogin(user.getId())).thenReturn(new String[] {"refresh-token", "access-token"});
 
 		// when
 		AuthResponse authResponse = authService.login(authRequest);
@@ -49,7 +48,6 @@ class AuthServiceTest {
 		// then
 		assertEquals("access-token", authResponse.accessToken());
 		assertEquals("refresh-token", authResponse.refreshToken());
-		verify(authRepository).save(any(Auth.class));
 	}
 
 	@Test
@@ -57,9 +55,11 @@ class AuthServiceTest {
 	void createTokensWhenLoginFailTest() {
 		// given
 		AuthRequest authRequest = new AuthRequest("user@example.com", "wrong-password");
-		User user = User.ofBuyer("user@example.com", "name", "password");
+		User user = User.ofBuyer(1L, "user@example.com", "name", "password");
 
 		when(userService.getUserByEmail(authRequest.email())).thenReturn(user);
+		when(userService.getUserById(1L)).thenReturn(user);
+		when(encryptHelper.isMatch(anyString(), anyString())).thenReturn(false);
 
 		// when & then
 		assertThrows(NotFoundException.class, () -> authService.login(authRequest));
