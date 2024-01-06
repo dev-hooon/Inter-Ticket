c57fd030673890123597a2193a1473a39b02a3f0
ParkJuHan94 <1004sunye@naver.com>
2024-01-06 12:29:48 +0900

    [feat]: JwtProvider 로직 변경해서 구현

--- core/src/main/java/dev/hooon/auth/application/JwtProvider.java
+++ core/src/main/java/dev/hooon/auth/application/JwtProvider.java
@@ -1,77 +1,106 @@
 package dev.hooon.auth.application;
 
-import static dev.hooon.auth.domain.entity.TokenType.*;
 import static dev.hooon.auth.exception.AuthErrorCode.*;
 
-import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import dev.hooon.auth.domain.entity.TokenType;
-import dev.hooon.auth.domain.entity.UserInfo;
+import dev.hooon.auth.domain.entity.Auth;
+import dev.hooon.auth.domain.repository.AuthRepository;
 import dev.hooon.auth.exception.AuthException;
-import dev.hooon.user.domain.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
 
-	public String createToken(User user, TokenType tokenType) {
-		Claims claims = Jwts.claims();
-		claims.put("id", user.getId());
-		claims.put("email", user.getEmail());
-		claims.put("name", user.getName());
+	private final AuthRepository authRepository;
+
+	@Value(value = "${jwt.secret}")
+	private String secretKey;
+
+	@Value(value = "${jwt.token-validity-in-seconds}")
+	private int tokenValidSeconds;
+
+	private Key key;
+	private static final String USER_ID = "userId";
+
+	@PostConstruct
+	public void init() {
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+		key = Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public String createAccessToken(Long userId) {
 		Date now = new Date();
 
 		return Jwts.builder()
 			.setHeaderParam("type", "jwt")
-			.setClaims(claims)
+			.claim(USER_ID, userId)
 			.setIssuedAt(now)
-			.setExpiration(new Date(now.getTime() + tokenType.getValidTime()))
-			.signWith(
-				Keys.hmacShaKeyFor(tokenType.getSecretKey().getBytes(StandardCharsets.UTF_8)),
-				SignatureAlgorithm.HS256
-			)
+			.setExpiration(new Date(now.getTime() + tokenValidSeconds))
+			.signWith(key, SignatureAlgorithm.HS256)
 			.compact();
 	}
 
-	public boolean validateToken(String token, TokenType tokenType) {
+	public String createRefreshToken(Long userId) {
+		Date now = new Date();
+
+		return Jwts.builder()
+			.setHeaderParam("type", "jwt")
+			.claim(USER_ID, userId)
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + tokenValidSeconds * 30L))
+			.signWith(key, SignatureAlgorithm.HS256)
+			.compact();
+	}
+
+	public String[] createTokensWhenLogin(Long userId){
+		String refreshToken = createRefreshToken(userId);
+		String accessToken = createAccessToken(userId);
+		Optional<Auth> auth = authRepository.findByUserId(userId);
+
+		auth.ifPresentOrElse(
+			(none) -> authRepository.updateRefreshToken(auth.get().getId(), refreshToken),
+			() -> {
+				Auth newAuth = Auth.of(userId, refreshToken);
+				authRepository.save(newAuth);
+			}
+		);
+
+		return new String[] {refreshToken, accessToken};
+	}
+
+	public void validateToken(String token) {
 		try {
-			return !Jwts.parserBuilder()
-				.setSigningKey(Keys.hmacShaKeyFor(tokenType.getSecretKey().getBytes(StandardCharsets.UTF_8)))
+			Jwts.parserBuilder()
+				.setSigningKey(key)
 				.build()
 				.parseClaimsJws(token)
-				.getBody()
-				.getExpiration().before(new Date());
+				.getBody();
 		} catch (Exception e) {
-			if(tokenType.equals(ACCESS)){
-				throw new AuthException(INVALID_ACCESS_TOKEN);
-			}else{
-				throw new AuthException(INVALID_REFRESH_TOKEN);
-			}
+			throw new AuthException(INVALID_TOKEN);
 		}
 	}
 
-	public UserInfo getClaim(String token, TokenType tokenType) {
+	public Long getClaim(String token) {
 		Claims claimsBody = Jwts.parserBuilder()
-			.setSigningKey(Keys.hmacShaKeyFor(tokenType.getSecretKey().getBytes(StandardCharsets.UTF_8)))
+			.setSigningKey(key)
 			.build()
 			.parseClaimsJws(token)
 			.getBody();
 
-		return UserInfo.of(
-			Long.valueOf((Integer)claimsBody.getOrDefault("id", 0L)),
-			claimsBody.getOrDefault("email", "").toString(),
-			claimsBody.getOrDefault("name", "").toString()
-		);
+		return Long.valueOf((Integer)claimsBody.getOrDefault(USER_ID, 0L));
 	}
 
 }
