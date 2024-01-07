0a7e2c9efd1e4765cf6ddd4eeed86126da110e35
ParkJuHan94 <1004sunye@naver.com>
2024-01-07 13:33:36 +0900

    [feat]: WaitingBookingApiControllerTest 되돌림

--- api/src/test/java/dev/hooon/waitingbooking/WaitingBookingApiControllerTest.java
+++ api/src/test/java/dev/hooon/waitingbooking/WaitingBookingApiControllerTest.java
@@ -1,34 +1,28 @@
 package dev.hooon.waitingbooking;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import dev.hooon.auth.application.JwtProvider;
-import dev.hooon.common.entity.DatabaseCleaner;
 import dev.hooon.common.support.ApiTestSupport;
 import dev.hooon.user.application.UserService;
-import dev.hooon.user.dto.request.UserJoinRequest;
+import dev.hooon.user.domain.entity.User;
 import dev.hooon.waitingbooking.domain.entity.WaitingBooking;
 import dev.hooon.waitingbooking.domain.repository.WaitingBookingRepository;
 import dev.hooon.waitingbooking.dto.request.WaitingRegisterRequest;
 
-@SpringBootTest
-@ActiveProfiles("test")
-@ComponentScan(basePackages = {"dev.hooon.waitingbooking", "dev.hooon.auth.application", "dev.hooon.common.entity"})
 @DisplayName("[WaitingBookingApiController API 테스트]")
 class WaitingBookingApiControllerTest extends ApiTestSupport {
 
@@ -36,30 +30,26 @@ class WaitingBookingApiControllerTest extends ApiTestSupport {
 	private MockMvc mockMvc;
 	@Autowired
 	private WaitingBookingRepository waitingBookingRepository;
-	@Autowired
+	// TODO User 저장에 대한 로직을 다른 작업에서 병행중이어서 일단 모킹으로 대체하구 추후에 수정
+	@MockBean
 	private UserService userService;
-	@Autowired
-	private JwtProvider jwtProvider;
-	@Autowired
-	private DatabaseCleaner databaseCleaner;
-
-	@BeforeEach
-	void setUp() {
-		databaseCleaner.clear();
-		userService.join(new UserJoinRequest("hello@example.com", "hello-name", "hello-password"));
-	}
 
 	@Test
 	@DisplayName("[예매대기 등록 API 를 호출하면 예매대기가 등록되고 예매대기 ID 를 응답한다]")
 	void registerWaitingBookingTest() throws Exception {
 		//given
-		String accessToken = jwtProvider.createAccessToken(1L);
+		// TODO 추후에 User 생성 기능이 구현되면 수정 예정
+		User user = new User();
+		ReflectionTestUtils.setField(user, "id", 1L);
+		given(userService.getUserById(1L)).willReturn(user);
+
 		WaitingRegisterRequest waitingRegisterRequest = new WaitingRegisterRequest(2, List.of(1L, 2L, 3L, 4L));
 
+		//when
 		ResultActions actions = mockMvc.perform(
 			MockMvcRequestBuilders
 				.post("/api/waiting_bookings")
-				.header("Authorization", accessToken) // Include JWT token in the request header
+				.queryParam("userId", "1") // TODO 추후에 인증정보 ArgumentResolver 가 구현되면 수정 예정
 				.contentType(APPLICATION_JSON)
 				.content(toJson(waitingRegisterRequest))
 		);
