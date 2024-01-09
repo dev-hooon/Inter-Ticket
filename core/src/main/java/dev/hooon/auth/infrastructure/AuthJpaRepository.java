3f2308820f35ccca41a7277ae1c50b057717a724
ParkJuHan94 <1004sunye@naver.com>
2024-01-09 18:18:44 +0900

    [feat]: AuthJpaRepository 정리

--- core/src/main/java/dev/hooon/auth/infrastructure/AuthJpaRepository.java
+++ core/src/main/java/dev/hooon/auth/infrastructure/AuthJpaRepository.java
@@ -1,18 +1,14 @@
 package dev.hooon.auth.infrastructure;
 
-import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import dev.hooon.auth.domain.entity.Auth;
-import dev.hooon.show.domain.entity.seat.SeatStatus;
 
-@Repository
 public interface AuthJpaRepository extends JpaRepository<Auth, Long> {
 	Optional<Auth> findByRefreshToken(String refreshToken);
 
