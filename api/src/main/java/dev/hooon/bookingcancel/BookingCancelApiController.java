649c2409b9f6455c4ff328e83e2235caf1ea5e83
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2024-01-10 20:56:32 +0900

    [refactor] : 인증필요한 API 에 인증 적용 + 스케줄러 테스트 간헐적으로 터지는 버그 해 (#70)
    * refactor : 인증이 필요한 API 에 인증 적용 및 테스트코드 수정
    
    * refactor : 스케줄러 테스트 간헐적으로 터지는 버그 해결

--- api/src/main/java/dev/hooon/bookingcancel/BookingCancelApiController.java
+++ api/src/main/java/dev/hooon/bookingcancel/BookingCancelApiController.java
@@ -3,13 +3,13 @@ package dev.hooon.bookingcancel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.hooon.auth.annotation.NoAuth;
+import dev.hooon.auth.jwt.JwtAuthorization;
 import dev.hooon.booking.application.BookingService;
 import dev.hooon.booking.dto.response.BookingCancelResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +21,11 @@ public class BookingCancelApiController {
 
 	private final BookingService bookingService;
 
-	@NoAuth
 	@PostMapping("/api/bookings/cancel/{booking_id}")
 	@Operation(summary = "예매 취소 API", description = "예매를 취소한다")
 	@ApiResponse(responseCode = "200", useReturnTypeSchema = true)
 	public ResponseEntity<BookingCancelResponse> cancelBooking(
-		@RequestParam(name = "userId") Long userId, // TODO
+		@Parameter(hidden = true) @JwtAuthorization Long userId,
 		@PathVariable("booking_id") Long bookingId
 	) {
 		BookingCancelResponse bookingCancelResponse = bookingService.cancelBooking(
