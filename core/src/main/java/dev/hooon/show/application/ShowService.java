47bb70c5240d4f5950b36dd6cfe1fc3e3df2dba9
ParkJuHan94 <1004sunye@naver.com>
2024-01-10 15:31:15 +0900

    [feat]:  getShowsByCategory() 구현

--- core/src/main/java/dev/hooon/show/application/ShowService.java
+++ core/src/main/java/dev/hooon/show/application/ShowService.java
@@ -2,19 +2,25 @@ package dev.hooon.show.application;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import dev.hooon.common.exception.NotFoundException;
 import dev.hooon.show.domain.entity.Show;
+import dev.hooon.show.domain.entity.ShowCategory;
 import dev.hooon.show.domain.repository.SeatRepository;
 import dev.hooon.show.domain.repository.ShowRepository;
 import dev.hooon.show.dto.ShowMapper;
 import dev.hooon.show.dto.query.SeatDateRoundDto;
 import dev.hooon.show.dto.response.AbleBookingDateRoundResponse;
 import dev.hooon.show.dto.response.ShowDetailsInfoResponse;
+import dev.hooon.show.dto.response.ShowInfoResponse;
 import dev.hooon.show.exception.ShowErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ShowService {
@@ -33,5 +39,17 @@ public class ShowService {
 			() -> new NotFoundException(ShowErrorCode.SHOW_NOT_FOUND)
 		);
 		return ShowMapper.toShowDetailInfoResponse(show);
+	}
+
+	public List<ShowInfoResponse> getShowsByCategory(int page, int size, String category) {
+		ShowCategory showCategory = ShowCategory.of(category);
+		PageRequest pageRequest = PageRequest.of(page, size);
+		Page<Show> showsPage = showRepository.findByCategory(showCategory, pageRequest);
+
+		if (showsPage.hasContent()) {
+			return showsPage.map(ShowInfoResponse::of).getContent();
+		} else {
+			return List.of();
+		}
 	}
 }
