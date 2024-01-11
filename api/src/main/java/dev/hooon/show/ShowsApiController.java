5c2e7bbdab1915b9c2aab64ad7288612f2f4a5cf
ParkJuHan94 <1004sunye@naver.com>
2024-01-11 17:03:55 +0900

    [feat]: ShowsApiController 에 스웨거 세팅 추가

--- api/src/main/java/dev/hooon/show/ShowsApiController.java
+++ api/src/main/java/dev/hooon/show/ShowsApiController.java
@@ -9,10 +9,14 @@ import dev.hooon.auth.annotation.NoAuth;
 import dev.hooon.common.dto.PagedResponse;
 import dev.hooon.show.application.ShowService;
 import dev.hooon.show.dto.response.ShowInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Show API")
 @RestController
 @RequiredArgsConstructor
 public class ShowsApiController {
@@ -21,6 +25,8 @@ public class ShowsApiController {
 
 	@NoAuth
 	@GetMapping("/api/shows")
+	@Operation(summary = "카테고리별로 공연 전체조회 API", description = "선택한 카테고리의 전체 공연의 공연 이름, 장소 이름을 조회한다")
+	@ApiResponse(responseCode = "200", useReturnTypeSchema = true)
 	public ResponseEntity<PagedResponse<ShowInfoResponse>> getShowsByCategory(
 		@RequestParam("page") @Min(0) int page,
 		@RequestParam("size") @Min(1) int size,
