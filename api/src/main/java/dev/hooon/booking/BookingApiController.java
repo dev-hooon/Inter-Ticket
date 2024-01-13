0f88601743d858b9fad6bb2f9b86ec59abe46d77
수지니 <sssssjin99@naver.com>
2024-01-13 17:08:56 +0900

    [refactor]: 리팩토링

--- api/src/main/java/dev/hooon/booking/BookingApiController.java
+++ api/src/main/java/dev/hooon/booking/BookingApiController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.hooon.auth.jwt.JwtAuthorization;
 import dev.hooon.booking.application.BookingService;
 import dev.hooon.booking.dto.response.BookingListResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,6 +22,8 @@ public class BookingApiController {
 	private final BookingService bookingService;
 
 	@GetMapping("/api/users/booking")
+	@Operation(summary = "예매 조회 API", description = "예매를 조회한다")
+	@ApiResponse(responseCode = "200", useReturnTypeSchema = true)
 	public ResponseEntity<BookingListResponse> getBookings(
 		@Parameter(hidden = true) @JwtAuthorization Long userId,
 		@RequestParam(name = "duration") int duration,
